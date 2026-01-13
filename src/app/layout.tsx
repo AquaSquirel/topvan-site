0cb57b7acd8997296ad4498a26af92b66c92eef0
AquaSquirel <91857421+AquaSquirel@users.noreply.github.com>
2026-01-13 00:15:05 -0300

    att: site title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-  title: "Topvan | Transporte Executivo Piedade - Sorocaba",
+  title: "Topvan | Transporte Executivo",
   description: "Transporte universitário e escolar diário entre Piedade e Sorocaba. Aluguel de vans executivas (Mercedes Sprinter) para turismo e eventos. Fundada por Juliano Amaral.",
 };
 
